d763a791540f5ee1c7e7d4a7d889b33e7af17b24
FalcoDJ <craftycoder1@gmail.com>
2020-09-02 09:36:05 -0400

    changed health bar to yellow

--- HealthBar.cpp
+++ HealthBar.cpp
@@ -4,8 +4,8 @@ void HealthBar::update(float health)
 {
   m_HealthBar.setSize(sf::Vector2f((health / 100) * 100, 6));
   m_HealthBar.setPosition(sf::Vector2f(8, 8));
-  m_HealthBar.setFillColor(sf::Color(255, 0, 0));
-  m_HealthBar.setOutlineColor(sf::Color(255, 255, 255));
+  m_HealthBar.setFillColor(sf::Color(255, 255, 0));
+  m_HealthBar.setOutlineColor(sf::Color(0, 0, 0));
   m_HealthBar.setOutlineThickness(1);
 
 }
